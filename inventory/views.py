c3527d6e6c77db327f863fd95aabbb0be279b1f1
Sunny Ruan <jiaqingr@gmail.com>
2018-10-20 21:09:50 -0400

    Error fixed; need to change database to be postgres

--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Inventory, Computer
 from .forms import computerForm, inventoryForm
 
+# List all of the inventories
 def index(request):
     all_inventories = Inventory.objects.all()
     context = {
@@ -12,16 +12,18 @@ def index(request):
     }
     return render(request, 'inventory/index.html', context)
 
-def inventory_detail(request, inventory_id, computer_id):
+# Show detail info of each computer that the inventory has.
+# Including manufacturer, serial number and comments
+def inventory_detail(request, inventory_id):
     inventory = get_object_or_404(Inventory, pk=inventory_id)
-    computer = get_object_or_404(Computer, pk=computer_id)
+    computer = Computer.objects.filter(inventory=inventory_id)
     context = {
         'inventory': inventory,
         'computer': computer
     }
- #   url = "/inventory" + str(inventory.id) + "/"
-    return render(request, 'inventory/detail.html', context)
+    return render(request, 'inventory/inventory_detail.html', context)
 
+# Function to add inventory
 def add_inventory(request):
     if request.method == "POST":
         form = inventoryForm(request.POST)
@@ -34,28 +36,42 @@ def add_inventory(request):
     context = { 'form': form }
     return render(request, 'inventory/add_inventory.html', context)
 
+# Function to add computer
 def add_computer(request, inventory_id):
     inventory = get_object_or_404(Inventory, pk=inventory_id)
     if request.method == "POST":
         form = computerForm(request.POST)
-        form.save()
-        url = "/inventory/" + str(inventory_id) + "/"
-        return HttpResponseRedirect(url)
+        # If the form is valid, then save it and return to the corresponding inventory page
+        if form.is_valid():
+            comp = form.save(commit=False)
+            comp.inventory = Inventory.objects.get(pk=inventory_id)
+            comp.save()
+            url = "/inventory/" + str(inventory_id) + "/"
+            return HttpResponseRedirect(url)
     else:
         form = computerForm()
-        return render(request, 'inventory/add_computer.html', {})
+    context = { 'form': form }
+    return render(request, 'inventory/add_computer.html', context)
 
+# Function to edit info of the computer
 def edit_computer(request, inventory_id, computer_id):
     computer = get_object_or_404(Computer, pk=computer_id)
     if request.method == "POST":
         form = computerForm(request.POST, instance=computer)
-        form.save()
-        url = "/inventory/" + str(inventory_id) + "/" + str(computer_id) + "/"
-        return HttpResponseRedirect(url)
+        # If the form is valid, then save it and return to the corresponding inventory page
+        if form.is_valid():
+            form.save()
+            url = "/inventory/" + str(inventory_id) + "/" + str(computer_id) + "/"
+            return HttpResponseRedirect(url)
     else:
         form = computerForm()
-        return render(request, 'inventory/edit.html', {})
+    context = { 
+        'form': form,
+        'computer': computer
+    }
+    return render(request, 'inventory/edit_computer.html', context)
 
+# Function to delete computer
 def delete_computer(request, inventory_id, computer_id):
     computer = get_object_or_404(Computer, pk=computer_id)
     if request.method == "POST":
